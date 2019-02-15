f996cb3cf2baef8c0ef618a0151b160355264adf
Alex Trask <Sarlack_Work@mail.ru>
2019-02-15 23:25:09 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
 import {render} from 'react-dom'
-import Main from './components/Main'
+import Main from './components/Main' 
 
 render(<Main/>, document.getElementById('root'))
